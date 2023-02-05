8d04a68c7c9810bf27697c66da807f0f197a7f5d
Zakir-Shaikh <122259194+Zakir-Shaikh@users.noreply.github.com>
2023-02-05 11:25:06 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using static ConsoleApp1.MLModel1;
+
+Console.WriteLine("taxi fare is!");
+//Load sample data
+var sampleData = new MLModel1.ModelInput()
+{
+    Vendor_id = @"VTS",
+    Rate_code = 1F,
+    Passenger_count = 1F,
+    Trip_time_in_secs = 480F,
+    Trip_distance = 2.72F,
+    Payment_type = @"CRD",
+};
+var input = new ModelInput();
+{
+    input.Trip_distance = 100;
+    input.Trip_time_in_secs = 1000;
+};
+
+//Load model and predict output
+var result = MLModel1.Predict(sampleData);
+Console.WriteLine(result.Score);
